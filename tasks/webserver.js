9e29d0e3c6905a663a0444e2423913233b07c5eb
feb0223 <feb0223@gmail.com>
2015-05-27 17:56:25 +0900

    modify webserver

--- tasks/webserver.js
+++ tasks/webserver.js
@@ -1,18 +1,19 @@
 var gulp = require('gulp');
 var webserver = require('gulp-webserver');
 
-module.exports = function(config, option) {
-	option = option || {};
+module.exports = function(config) {
+	var options = config.options || {};
+	
+	if (options.directoryListing === true) {
+		options.directoryListing = {enable: true};
+	} else if (typeof(options.directoryListing) !== 'object') {
+		options.directoryListing = {enable: false};
+	}
+	options.directoryListing.path = config.rootDir;
+	options.port = options.port || 3000;
 	
 	return function() {
 		gulp.src(config.rootDir)
-			.pipe(webserver({
-				host:'0.0.0.0',
-				directoryListing: {
-					enable: true,
-					path: config.rootDir
-				},
-				port: config.port || 3000
-			}));
+			.pipe(webserver(options));
 	};
 };
